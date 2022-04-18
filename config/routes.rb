0d47340049e2cf3e6acb150492984ec4d53b6577
mile <milenajohanna1@gmail.com>
2022-04-17 21:17:55 -0300

    pagina edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'users#index'
   get '/contact', to: 'users#new', as: "new_user"
   post '/contact', to: 'users#create', as: "users"
+  get '/edit', to: 'users#edit'
 end
